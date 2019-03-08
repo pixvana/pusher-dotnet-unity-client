e512a434105af2458c1e0421be0f5f8784d99ca4
Kaustubh Garimella <kaustubh@pixvana.com>
2019-03-08 13:01:24 -0800

    http auth supports json + user data

--- UnityClient/Assets/Scripts/PusherClient/HttpAuthorizer.cs
+++ UnityClient/Assets/Scripts/PusherClient/HttpAuthorizer.cs
@@ -11,16 +11,18 @@ namespace PusherClient
     {
         private readonly Uri _authEndpoint;
         private string _userId;
+        private string _userData;
         private IDictionary<string, string> _headers = new Dictionary<string, string>();
 
         /// <summary>
         /// ctor
         /// </summary>
         /// <param name="authEndpoint">The End point to contact</param>
-        public HttpAuthorizer(string authEndpoint, string userId = null, IDictionary<string, string> headers = null)
+        public HttpAuthorizer(string authEndpoint, string userId = null, string userData = null, IDictionary<string, string> headers = null)
         {
             _authEndpoint = new Uri(authEndpoint);
             _userId = userId;
+            _userData = userData;
             if (headers != null)
             {
                 _headers = headers;
@@ -44,14 +46,14 @@ namespace PusherClient
                 {
                     data = data + $"&user_id={_userId}";
                 }
-                webClient.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
 
                 foreach (KeyValuePair<string, string> header in _headers)
                 {
                     webClient.Headers[header.Key] = header.Value;
                 }
 
-                authToken = webClient.UploadString(_authEndpoint, "POST", data);
+                authToken = webClient.UploadString(_authEndpoint + "?" + data, "POST", _userData);
             }
 
             return authToken;
